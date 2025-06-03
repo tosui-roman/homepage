3a12efc6001962060b2430344817eedfde7f3e28
tosui-roman <abcrmrm424@gmail.com>
2025-06-03 23:00:11 +0900

    初回

--- yukina.config.ts
+++ yukina.config.ts
@@ -8,7 +8,7 @@ const YukinaConfig: Configuration = {
 
   description: "Demo Site",
 
-  site: "https://yukina-blog.vercel.app",
+  site: "https://tosui-roman.github.io",
 
   locale: "en", // set for website language and date format
 
