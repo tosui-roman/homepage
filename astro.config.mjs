fdd797768817283bc73aa2c119009815b037da0d
tosui-roman <abcrmrm424@gmail.com>
2025-06-03 23:29:44 +0900

    deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -19,7 +19,7 @@ import pagefind from "astro-pagefind";
 // https://astro.build/config
 export default defineConfig({
   site: YukinaConfig.site,
-  base: "/homepage/",
+  base: "/",
   integrations: [
     tailwind(),
     svelte(),
